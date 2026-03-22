e11f6e6317c2a0163c763c61b47face2a88b01a9
Hacker-shivam <saurabhsingh847101@gmail.com>
2026-03-22 18:05:52 +0530

    update

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://e-booking-dcra.onrender.com/api', 
+    baseURL: 'https://e-booking-cnyl.onrender.com/api', 
     headers:{
     'Content-Type': 'application/json',
     },
